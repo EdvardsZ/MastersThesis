653cd91764f63c2b19a919f851b6431abed3ed4f
EdvardsZ <edvards1999@gmail.com>
2023-02-21 00:51:35 +0100

    update encoder creation

--- tutorials/semi-conditional-variational-autoencoder/SCVAE.py
+++ tutorials/semi-conditional-variational-autoencoder/SCVAE.py
@@ -2,34 +2,38 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self,kernel_size=2, filters=128, latent_dim=2):
+    def __init__(self,kernel_size=2, hidden_dims = [ 128, 256], latent_dim=2):
         super(Encoder, self).__init__()
-        self.filters = filters
+
         self.kernel_size = kernel_size
         self.latent_dim = latent_dim
+        self.hidden_dims = hidden_dims
 
-        # Encoder input layer
-        # padding = "same" and stride = 2 not supported in PyTorch
-        # https://discuss.pytorch.org/t/conv2d-padding-same-and-stride-2/4691/2
-        self.inputs = nn.Sequential(
-            nn.Conv2d(1, self.filters, kernel_size=self.kernel_size, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(self.filters, self.filters*2, kernel_size=self.kernel_size, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Flatten(),
-        )
+        in_channels = 1
+        modules = []
+        # Build Encoder
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim,
+                              kernel_size= 3, stride= 2, padding  = 1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
 
-        # Latent vector layer
-        self.z_mean = nn.Linear(16, latent_dim, bias=True)
-        self.z_log_var = nn.Linear(16, latent_dim, bias=True)
-        self.z = nn.Sequential(nn.Linear(latent_dim, latent_dim))
+        modules.append(nn.Flatten())
 
-    def forward_cnn(self, inputs):
-        x = self.inputs(inputs)
-        z_mean = self.z_mean(x)
-        z_log_var = self.z_log_var(x)
-        z = self.z(z_mean)
-        return z_mean, z_log_var, z
+        self.encoder = nn.Sequential(*modules)
+
+        self.fc_mu = nn.Linear(12544, latent_dim)
+        self.fc_var = nn.Linear(12544, latent_dim)
+
+    def forward(self, inputs):
+        x = self.encoder(inputs)
+        z_mean = self.fc_mu(x)
+        z_log_var = self.fc_var(x)
+        return x, z_mean, z_log_var
 
 
 encoder = Encoder(28)
@@ -40,16 +44,12 @@ train_loader, test_loader, val_loader  = load_mnist(BATCH_SIZE=128)
 
 example = next(iter(train_loader))[0]
 
-res = encoder.forward_cnn(example)
+print(example.shape)
+
+res = encoder.forward(example)
 
 print(res[0].shape)
 
 print(res[1].shape)
 
 print(res[2].shape)
-
-print(res[0])
-
-print(res[1])
-
-print(res[2])
--- tutorials/semi-conditional-variational-autoencoder/kerastest.py
+++ tutorials/semi-conditional-variational-autoencoder/kerastest.py
@@ -1,6 +1,6 @@
-from tensorflow import keras
+from keras.datasets import mnist
 import numpy as np
-
+from keras.layers import Reshape, Input
 
 def Conditioning(data, obs_x, obs_y, fill_value=0.000001):
     Cond = np.zeros((data.shape))
@@ -27,7 +27,7 @@ latent_dim = 2
 image_size = 28
 input_shape = (image_size, image_size, 1)
 
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 ## Extract the observations to condition on
 cond_x_train = Conditioning(x_train, obs_x, obs_y)
@@ -35,6 +35,14 @@ Cond_X_train=np.expand_dims(cond_x_train, axis=-1)
 cond_x_test = Conditioning(x_test, obs_x, obs_y)
 Cond_X_test=np.expand_dims(cond_x_test, axis=-1)
 
-print(cond_x_train.shape)
-print(Cond_X_train.shape)
-print(Cond_X_test.shape)
+x_train = np.reshape(x_train, [-1, image_size, image_size,1])
+x_test = np.reshape(x_test, [-1, image_size, image_size,1])
+
+inputs = Input(shape=input_shape, name='encoder_input')
+x = Reshape((image_size, image_size, 1))(inputs)
+
+# push x_train through the reshaper
+
+x_train = Reshape((image_size, image_size, 1))(x_train)
+
+print(x_train.shape)
