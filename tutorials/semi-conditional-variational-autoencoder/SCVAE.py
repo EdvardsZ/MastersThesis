24d9454c31308ad80a1ef81f6b43f547615fbc40
EdvardsZ <edvards1999@gmail.com>
2023-02-27 22:27:25 +0100

    update

--- tutorials/semi-conditional-variational-autoencoder/SCVAE.py
+++ tutorials/semi-conditional-variational-autoencoder/SCVAE.py
@@ -5,21 +5,22 @@ import torch.nn as nn
 # ENCODER
 
 class Encoder(nn.Module):
-    def __init__(self,kernel_size=3, hidden_dims = [ 128, 256], latent_dim=2):
+    def __init__(self, image_size = (1, 28, 28), hidden_dims = [128, 256], latent_dim=2):
         super(Encoder, self).__init__()
 
-        self.kernel_size = kernel_size
+        self.image_size = image_size
         self.latent_dim = latent_dim
         self.hidden_dims = hidden_dims
 
-        in_channels = 1
+        in_channels = image_size[0]
+
         modules = []
         # Build Encoder
         for h_dim in hidden_dims:
             modules.append(
                 nn.Sequential(
                     nn.Conv2d(in_channels, out_channels=h_dim,
-                              kernel_size=kernel_size, stride= 2, padding  = 1),
+                              kernel_size=3, stride= 2, padding  = 1),
                     nn.BatchNorm2d(h_dim),
                     nn.LeakyReLU())
             )
@@ -27,10 +28,12 @@ class Encoder(nn.Module):
 
         modules.append(nn.Flatten())
 
+        resulting_size = (image_size[1] // 2**len(hidden_dims))**2 * hidden_dims[-1]
+
         self.encoder = nn.Sequential(*modules)
 
-        self.fc_mu = nn.Linear(12544, latent_dim) # To do calculate dimension size dynamicyally
-        self.fc_var = nn.Linear(12544, latent_dim)
+        self.fc_mu = nn.Linear(resulting_size, latent_dim) # To do calculate dimension size dynamicyally
+        self.fc_var = nn.Linear(resulting_size, latent_dim)
 
 
     def forward(self, inputs):
@@ -49,7 +52,7 @@ class Encoder(nn.Module):
 # CONDITONAL DECODER
 
 class ConditionalDecoder(nn.Module):
-    def __init__(self, kernel_size=3, hidden_dims = [256, 128], latent_dim=2):
+    def __init__(self, image_size = (1, 28, 28), kernel_size=3, hidden_dims = [256, 128], latent_dim=2):
         super(ConditionalDecoder, self).__init__()
         self.image_size = 28
 
