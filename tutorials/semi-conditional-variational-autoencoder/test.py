36ca4a552f804d702e76750b10a1306fc763ca3f
EdvardsZ <edvards1999@gmail.com>
2023-02-28 08:46:15 +0100

    update

--- tutorials/semi-conditional-variational-autoencoder/test.py
+++ tutorials/semi-conditional-variational-autoencoder/test.py
@@ -2,6 +2,14 @@ import torch
 
 import torch.nn as nn
 
+image = torch.randn(128, 256, 7, 7)
+
 inverter = nn.Sequential(
-    nn.ConvTranspose2d(2, hidden_dims[0], kernel_size=3, stride=2, padding=1, output_padding=1)
+    nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+    nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+    nn.Conv2d(128, out_channels= 1, kernel_size= 3, padding= 1),
 )
+
+
+
+print(inverter(image).shape)
