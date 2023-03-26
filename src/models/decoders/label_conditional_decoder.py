22feb9308766ff947133d050e4b116f6b44193eb
EdvardsZ <edvards1999@gmail.com>
2023-03-26 12:23:16 +0200

    update

--- src/models/decoders/label_conditional_decoder.py
+++ src/models/decoders/label_conditional_decoder.py
@@ -2,18 +2,19 @@
 import torch
 import torch.nn as nn
 
-class ConditionalDecoder(nn.Module):
-    def __init__(self, image_size = (1, 28, 28), hidden_dims = [256, 128], latent_dim=2):
-        super(ConditionalDecoder, self).__init__()
+class LabelConditionalDecoder(nn.Module):
+    def __init__(self, image_size = (1, 28, 28), hidden_dims = [256, 128], latent_dim=2, classes_count = 10):
+        super(LabelConditionalDecoder, self).__init__()
         self.image_size = image_size
         self.latent_dim = latent_dim
-        
-        resulting_size = (image_size[1] // 2**len(hidden_dims))**2 * hidden_dims[0]
-        in_channels = image_size[0]
+        self.classes = classes_count
+        self.hidden_dims = hidden_dims
 
+        resulting_size = (image_size[1] // 2**len(hidden_dims))**2 * hidden_dims[0]
+        
         self.decoder_input = nn.Sequential(
-            nn.Linear(self.image_size[1] * self.image_size[1] + latent_dim, resulting_size),
-            nn.LeakyReLU(),
+            nn.Linear(self.image_size[1] * self.image_size[1] + classes_count, resulting_size),
+            nn.ReLU(),
             nn.BatchNorm1d(resulting_size),
             nn.Unflatten(1, (hidden_dims[0], int(image_size[1]/(2**len(hidden_dims))), int(image_size[2]/(2**len(hidden_dims)))))
         )
@@ -41,15 +42,14 @@ class ConditionalDecoder(nn.Module):
         )
 
         self.decoder_output = nn.Sequential(*modules)
-        
+
         return
     
-    def forward(self, z, cond_input):
-        x_cond = torch.flatten(cond_input, start_dim=1)
-        x_cat = torch.cat((z, x_cond), dim=1)
+    def forward(self, z, label):
+        label = torch.nn.functional.one_hot(label, self.classes).float()
+        x = torch.cat((z, label), 1)
+        x = self.decoder_input(x)
+        x = self.decoder_output(x)
+        return x
 
-        input = self.decoder_input(x_cat)
-
-        output = self.decoder_output(input)
-
-        return output
+    
