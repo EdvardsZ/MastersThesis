d80c85e1983d2b3b8b23ad7cc91e8403f81ec9fd
EdvardsZ <edvards1999@gmail.com>
2023-04-10 19:41:39 +0200

    update

--- src/models/decoders/linear_decoder.py
+++ src/models/decoders/linear_decoder.py
@@ -2,19 +2,17 @@ import torch
 import torch.nn as nn
 
 class LinearDecoder(nn.Module):
-    def __init__(self, latent_dim, hidden_dim1, hidden_dim2, output_dim):
+    def __init__(self, image_size = (1, 28, 28), hidden_dims = [256, 512], latent_dim = 2):
         super(LinearDecoder, self).__init__()
-        self.latent_dim = latent_dim
-        self.hidden_dim1 = hidden_dim1
-        self.hidden_dim2 = hidden_dim2
-        self.output_dim = output_dim
+
+        output_dim = image_size[0] * image_size[1] * image_size[2]
 
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, hidden_dim1),
+            nn.Linear(latent_dim, hidden_dims[0]),
             nn.ReLU(),
-            nn.Linear(hidden_dim1, hidden_dim2),
+            nn.Linear(hidden_dims[0], hidden_dims[1]),
             nn.ReLU(),
-            nn.Linear(hidden_dim2, output_dim),
+            nn.Linear(hidden_dims[1], output_dim),
             nn.Sigmoid()
         )
 
--- src/models/encoders/linear_encoder.py
+++ src/models/encoders/linear_encoder.py
@@ -2,21 +2,20 @@ import torch
 import torch.nn as nn
 
 class LinearEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim1, hidden_dim2, latent_dim):
+    def __init__(self, image_size = (1, 28, 28), hidden_dims = [512, 256], latent_dim = 2):
         super(LinearEncoder, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim1 = hidden_dim1
-        self.hidden_dim2 = hidden_dim2
-        self.latent_dim = latent_dim
+
+        input_dim = image_size[0] * image_size[1] * image_size[2]
+
  
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim1),
+            nn.Linear(input_dim, hidden_dims[0]),
             nn.ReLU(),
-            nn.Linear(hidden_dim1, hidden_dim2),
+            nn.Linear(hidden_dims[0], hidden_dims[1]),
             nn.ReLU(),
         )
-        self.z_mean = nn.Linear(hidden_dim2, latent_dim)
-        self.z_log_var = nn.Linear(hidden_dim2, latent_dim)
+        self.z_mean = nn.Linear(hidden_dims[1], latent_dim)
+        self.z_log_var = nn.Linear(hidden_dims[1], latent_dim)
 
     def forward(self, x):
 
