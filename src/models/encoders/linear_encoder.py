cdcbd5d15d5f57319824456369e46f3def1bbb5f
EdvardsZ <edvards1999@gmail.com>
2023-04-10 19:06:10 +0200

    update

--- src/models/encoders/linear_encoder.py
+++ src/models/encoders/linear_encoder.py
@@ -2,46 +2,29 @@ import torch
 import torch.nn as nn
 
 class LinearEncoder(nn.Module):
-    def __init__(self, image_size = (1, 28, 28), n_hidden=500, latent_dim=2, keep_prob=0.99):
+    def __init__(self, input_dim, hidden_dim1, hidden_dim2, latent_dim):
         super(LinearEncoder, self).__init__()
-
-        self.image_size = image_size
+        self.input_dim = input_dim
+        self.hidden_dim1 = hidden_dim1
+        self.hidden_dim2 = hidden_dim2
         self.latent_dim = latent_dim
+
+        # encoder part
+        self.fc1 = nn.Linear(input_dim, hidden_dim1)
+        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2)
+        self.fc31 = nn.Linear(hidden_dim2, latent_dim)
+        self.fc32 = nn.Linear(hidden_dim2, latent_dim)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+
+        x = self.relu(self.fc1(x))
+
+        x = self.relu(self.fc2(x))
+
+        z_mean = self.fc31(x)
+        z_log_var = self.fc32(x)
         
-        in_channels = self.image_size[0]
-
-        flat_size = image_size[1] * image_size[2]
-
-        self.encoder =  nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(flat_size, n_hidden),
-            nn.ELU(inplace=True),
-            nn.Dropout(1-keep_prob),
-
-            nn.Linear(n_hidden, n_hidden),
-            nn.Tanh(),
-            nn.Dropout(1-keep_prob),
-        )
-
-        self.fc_mu = nn.Linear(n_hidden, latent_dim)
-
-        self.fc_var = nn.Linear(n_hidden, latent_dim)
-
-    def forward(self, inputs):
-        x = self.encoder(inputs)
-        z_mean = self.fc_mu(x)
-        z_log_var = self.fc_var(x)
-        z = self.sampling(z_mean, z_log_var)
+        return z_mean, z_log_var
         
-        return z_mean, z_log_var, z
-    
-    def sampling(self, z_mean, z_log_var):
-        eps = torch.randn_like(z_log_var)
-        z = z_mean + torch.exp(z_log_var / 2) * eps
-        return z
-    
-
-
-
-
-
