4da40634c66e80fe13d2c5a9180529ae6dd29fd5
EdvardsZ <edvards1999@gmail.com>
2023-04-10 19:17:30 +0200

    update

--- src/models/encoders/linear_encoder.py
+++ src/models/encoders/linear_encoder.py
@@ -8,23 +8,22 @@ class LinearEncoder(nn.Module):
         self.hidden_dim1 = hidden_dim1
         self.hidden_dim2 = hidden_dim2
         self.latent_dim = latent_dim
-
-        # encoder part
-        self.fc1 = nn.Linear(input_dim, hidden_dim1)
-        self.fc2 = nn.Linear(hidden_dim1, hidden_dim2)
-        self.fc31 = nn.Linear(hidden_dim2, latent_dim)
-        self.fc32 = nn.Linear(hidden_dim2, latent_dim)
-
-        self.relu = nn.ReLU()
+ 
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim1),
+            nn.ReLU(),
+            nn.Linear(hidden_dim1, hidden_dim2),
+            nn.ReLU(),
+        )
+        self.z_mean = nn.Linear(hidden_dim2, latent_dim)
+        self.z_log_var = nn.Linear(hidden_dim2, latent_dim)
 
     def forward(self, x):
 
-        x = self.relu(self.fc1(x))
+        x = self.encoder(x)
 
-        x = self.relu(self.fc2(x))
-
-        z_mean = self.fc31(x)
-        z_log_var = self.fc32(x)
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
         
         return z_mean, z_log_var
         
