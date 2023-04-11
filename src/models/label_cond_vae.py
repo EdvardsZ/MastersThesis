e893d22e9a9a28b27d9f6e0c8e8a880c487b61cb
EdvardsZ <edvards1999@gmail.com>
2023-04-11 08:27:35 +0200

    update

---
+++ src/models/label_cond_vae.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from models.decoders import Decoder
+from models.encoders import Encoder
+
+from loss.vae_loss import VAELoss
+
+class LabelConditionedVAE(nn.Module):
+    def __init__(self, kernel_size=3, hidden_dims = [128, 256], latent_dim=2):
+        super(LabelConditionedVAE, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.latent_dim = latent_dim
+
+        self.loss = VAELoss(weight_kl=1.0)
+        
+    def forward(self, inputs):
+        z_mean, z_log_var, z = self.encoder(inputs)
+        output = self.decoder(z)
+        return output, z_mean, z_log_var, z
