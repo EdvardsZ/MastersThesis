6481278153551b93f9bee27f2dd147d2f0fbc69a
EdvardsZ <edvards1999@gmail.com>
2023-05-23 10:50:04 +0200

    soft adapt second try

--- src/loss/soft_adapt.py
+++ src/loss/soft_adapt.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from loss.functional.helpers import kl_loss, recon_loss
+import numpy as np
 
 class SoftAdaptVAELoss(nn.Module):
     def __init__(self, n, variant=[], beta=0.1):
@@ -15,42 +16,50 @@ class SoftAdaptVAELoss(nn.Module):
         recon = recon_loss(inputs, outputs)
         kl = kl_loss(z_mean, z_log_var)
 
-        self.update_losses(recon + kl)
+        self.update_losses(recon, kl)
 
-        weighted_loss = self.weighted_loss([recon + kl])
+        weighted_loss = self.weighted_loss(recon, kl)
 
         return {'recon_loss': recon, 'kl_loss': kl, 'loss': weighted_loss, "loss(no_weights)": recon + kl}
 
-    def update_losses(self, loss):
-        self.loss_buffer.append(loss)
+    def update_losses(self, loss_1, loss_2):
+        loss_1 = loss_1.detach().cpu().numpy()
+        loss_2 = loss_2.detach().cpu().numpy()
+        losses = np.array([loss_1, loss_2])
+        self.loss_buffer.append(losses)
         if len(self.loss_buffer) > self.n:
             self.loss_buffer.pop(0)
 
-    def weighted_loss(self, losses):
-        # Compute the rate of change and average of previous losses
+    def weighted_loss(self, loss_1, loss_2):
         if len(self.loss_buffer) < self.n:
-            return torch.sum(torch.stack(losses))
+            return loss_1 + loss_2
+        # Calculate the rates of change and averages of previous losses
+        si = np.gradient(self.loss_buffer, axis=0)[-1]
+        fi = np.mean(self.loss_buffer, axis=0)
 
-        delta_l = []
-        for i in range(1, len(self.loss_buffer)):
-            delta_l.append(self.loss_buffer[i] - self.loss_buffer[i-1])
-        si = torch.stack(delta_l)
-        fi = torch.mean(torch.stack(self.loss_buffer))
+        # Calculate the normalized rates of change
+        nsi = si / (np.sum(np.abs(si)) + 1e-8)
 
-        # Compute the SoftAdapt weights
-        beta = self.beta
+        # Calculate the max values of rates of change
+        max_si = np.max(si)
+        max_nsi = np.max(nsi)
+
+        # Calculate the exponentiated values of rates of change
+        exp_si = np.exp(self.beta * (nsi - max_nsi))
+        exp_fi = np.exp(self.beta * (fi - max_si))
+
+        # Calculate the sum of exponentiated values
+        sum_exp_si = np.sum(exp_si)
+        sum_exp_fi = np.sum(exp_fi)
+
+        # Calculate the weights based on the variant
         if "Normalized" in self.variant:
-            nsi = si / torch.abs(si).sum() + 1e-8
-            exp_sum = torch.sum(torch.exp(beta * (nsi - torch.max(nsi)))) + 1e-8
-            alpha = [torch.exp(beta * (ns_i - torch.max(nsi))) / exp_sum for ns_i in nsi]
+            alpha_i = exp_si / (sum_exp_si + 1e-8)
         else:
-            exp_sum = torch.sum(torch.exp(beta * (si - torch.max(si)))) + 1e-8
-            alpha = [torch.exp(beta * (s_i - torch.max(si))) / exp_sum for s_i in si]
+            alpha_i = exp_fi / (sum_exp_fi + 1e-8)
 
-        if "Loss Weighted" in self.variant:
-            avg_l = torch.mean(torch.stack(self.loss_buffer))
-            alpha = [(alpha_i * self.loss_buffer[i]) / avg_l for i, alpha_i in enumerate(alpha)]
+        # Calculate the weighted loss
+        weighted_loss = loss_1 * alpha_i[0] + loss_2 * alpha_i[1]
 
-        # Compute the weighted loss
-        weighted_loss = torch.sum(torch.stack([alpha[i] * losses[i] for i in range(len(losses))]))
         return weighted_loss
+        
