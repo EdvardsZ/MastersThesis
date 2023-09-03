9305752fbc14866cde3057b40df1081860c93d95
EdvardsZ <edvards1999@gmail.com>
2023-09-03 12:04:31 +0200

    Added simple vq decoder/ encoder

---
+++ src/models/decoders/simple_vq_decoder.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class SimpleVQDecoder:
+    def __init__(self, latent_dim=16):
+        super(SimpleVQDecoder, self).__init__()
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(latent_dim, 64, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 1, 3, padding=1)
+        )
+
+    def forward(self, x):
+        return self.decoder(x)
---
+++ src/models/encoders/simple_vq_encoder.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class SimpleVQEncoder:
+    def __init__(self, latent_dim=16):
+        super(SimpleVQEncoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, latent_dim, 1, padding=0)
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
