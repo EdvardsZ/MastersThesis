3be7f2f1e66ee6a14ff61e39b16ff25897af0d55
EdvardsZ <edvards1999@gmail.com>
2023-10-23 17:25:22 +0200

    Label mtvae WIP

--- src/loss/vae_loss.py
+++ src/loss/vae_loss.py
@@ -10,17 +10,38 @@ class VAELoss(nn.Module):
         self.loss_type = loss_type
 
     def forward(self, inputs, outputs):
-        reconstructions, mu, z_log_var, z_mean = outputs
+        classification = None
+        try:
+            reconstructions, mu, z_log_var, z_mean, classification = outputs
+        except:
+            reconstructions, mu, z_log_var, z_mean = outputs
+
+        loss_dict = {}
+        loss = 0
+
         x, x_cond, y = inputs
 
-        if self.loss_type == 'single':
-            recon = recon_loss(x, reconstructions)
-            kl = kl_loss(z_mean, z_log_var)
-            return { 'recon_loss': recon, 'kl_loss': kl, 'loss': recon + self.weight_kl * kl }
-        if self.loss_type == 'double':
-            recon_loss_1 = recon_loss(x, reconstructions[0])
-            recon_loss_2 = recon_loss(x, reconstructions[1])
-            kl = kl_loss(z_mean, z_log_var)
-            return { 'recon_loss': recon_loss_1, 'recon_loss_2': recon_loss_2, 'kl_loss': kl, 'loss': recon_loss_1 + recon_loss_2 + self.weight_kl * kl }
+        if isinstance(reconstructions, list):
+            recon_0 = recon_loss(x, reconstructions[0])
+            loss_dict['recon_loss'] = recon_0
+            loss += recon_0
+            recon_1 = recon_loss(x, reconstructions[1])
+            loss_dict['recon_loss_2'] = recon_1
+            loss += recon_1
+        else:
+            recon_0 = recon_loss(x, reconstructions)
+            loss_dict['recon_loss'] = recon_0
+            loss += recon_0
         
-        raise Exception('Invalid loss type')
+        kl = kl_loss(z_mean, z_log_var)
+        loss_dict['kl_loss'] = kl
+        loss += kl
+
+        if classification is not None:
+            classification = nn.CrossEntropyLoss(reduction='sum')(classification, y)
+            loss_dict['classification_loss'] = classification
+            loss += classification
+
+        loss_dict['loss'] = loss
+
+        return loss_dict
