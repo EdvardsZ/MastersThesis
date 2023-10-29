7ddd5614f4814ed56ef8c00de2d7c0917d14e68f
EdvardsZ <edvards1999@gmail.com>
2023-10-29 18:13:37 +0100

    Update

--- src/datasets/observations.py
+++ src/datasets/observations.py
@@ -5,25 +5,28 @@ from PIL import Image
 import torchvision.transforms as transforms
 
 class PartialObservation:
-    def __init__(self, conditioning_mode: str):
+    def __init__(self, conditioning_mode: str, add_mask: bool | None = None):
         self.conditioning_mode = conditioning_mode
 
+        self.add_mask = add_mask
+
+        if add_mask is None and conditioning_mode == "random":
+            self.add_mask = True
+
     def get_partial_observation(self, image: torch.Tensor) -> torch.Tensor:
-        data = self.image_to_tensor(image)
-        self.image_shape = data.shape
+        self.image_shape = image.shape
 
         obs_x, obs_y = self.get_observation_pixels()
 
-        if self.conditioning_mode == "exact":
-            cond_data = torch.zeros_like(data)
-            cond_data[:, obs_x, obs_y] = data[:, obs_x, obs_y]
+        if self.add_mask == False or self.add_mask is None:
+            cond_data = torch.zeros_like(image)
+            cond_data[:, obs_x, obs_y] = image[:, obs_x, obs_y]
             return cond_data
-        if self.conditioning_mode == "random": 
-            cond_data = torch.zeros((data.shape[0] + 1, data.shape[1], data.shape[2]))
-            cond_data[1:, obs_x, obs_y] = data[:, obs_x, obs_y]
+        else:
+            cond_data = torch.zeros((image.shape[0] + 1, image.shape[1], image.shape[2]))
+            cond_data[1:, obs_x, obs_y] = image[:, obs_x, obs_y]
             cond_data[0, obs_x, obs_y] = 1
             return cond_data
-        raise ValueError("conditioning_mode must be exact or random!!!")
 
     def get_observation_pixels(self) -> Tuple[List[int], List[int]]:
         if self.conditioning_mode == "exact":
@@ -66,7 +69,7 @@ class PartialObservation:
             obs_y.append(rand[i]% self.image_shape[2])
         return obs_x, obs_y
     
-    def image_to_tensor(self, image: Image.Image) -> torch.Tensor:
+    def image_to_tensor(self, image: torch.Tensor) -> torch.Tensor:
         image = transforms.ToPILImage()(image)
         data = transforms.ToTensor()(image)
         return data
