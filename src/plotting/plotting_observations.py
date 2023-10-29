2c28a64857debbb498b567d7dadeb7c51d8caf85
EdvardsZ <edvards1999@gmail.com>
2023-10-29 18:35:21 +0100

    Update plotting observations

--- src/plotting/plotting_observations.py
+++ src/plotting/plotting_observations.py
@@ -12,15 +12,28 @@ def plot_conditioned_examples(example: torch.Tensor):
     plot_conditioned_example(example, "random")
 
 
-def plot_conditioned_example(example: torch.Tensor, conditioning_mode: str):
-    partial_observation = PartialObservation(conditioning_mode)
+def plot_conditioned_example(example: torch.Tensor, conditioning_mode: str, save = False):
+    partial_observation = PartialObservation(conditioning_mode, add_mask = True)
 
-    plt.subplot(1, 2, 1)
+    plt.subplot(1, 3, 1)
     reshaped = example.detach().cpu().numpy().reshape(example.shape[1], example.shape[2])
     plt.imshow(reshaped)
     plt.axis('off')
 
     x_cond = partial_observation.get_partial_observation(example)
-    
 
-    plt.subplot(1, 2, 2)
+    plt.subplot(1, 3, 2)
+    reshaped = x_cond[0].detach().cpu().numpy().reshape(example.shape[1], example.shape[2])
+    plt.imshow(reshaped)
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    reshaped = x_cond[1].detach().cpu().numpy().reshape(example.shape[1], example.shape[2])
+    plt.imshow(reshaped)
+    plt.axis('off')
+
+    #save image
+    if save:
+        plt.savefig(f"assets/observations/{conditioning_mode}.png")
+
+    plt.show()
