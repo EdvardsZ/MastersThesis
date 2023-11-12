542a01627265a478620d635c3513242b05e15c73
EdvardsZ <edvards1999@gmail.com>
2023-11-12 22:15:57 +0100

    Update

--- src/datasets/dataset_loader.py
+++ src/datasets/dataset_loader.py
@@ -1,20 +1,26 @@
 import torch
 from torchvision import transforms
 from .conditional_dataset import ConditionalDataset
+from datasets.observations import CountSamplingMethod, PixelSamplingMethod
 
 from typing import Tuple
 
 def load_dataset(data_config: dict) -> Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, torch.utils.data.DataLoader, Tuple[int, int, int]]:
     BATCH_SIZE = data_config['batch_size']
     dataset = data_config['dataset']
+    count_sampling = data_config['count_sampling']
+    pixel_sampling = data_config['pixel_sampling']
+
+    count_sampling = CountSamplingMethod(count_sampling)
+    pixel_sampling = PixelSamplingMethod(pixel_sampling)
 
 
     preprocess = transforms.Compose([
         transforms.ToTensor()
     ])
 
-    train_set = ConditionalDataset(train=True, transform=preprocess, dataset = dataset)
-    test_val_set = ConditionalDataset(train=False, download=True, transform=preprocess, dataset = dataset)
+    train_set = ConditionalDataset(train=True, transform=preprocess, dataset = dataset, count_sampling = count_sampling, pixel_sampling = pixel_sampling)
+    test_val_set = ConditionalDataset(train=False, transform=preprocess, dataset = dataset, count_sampling = count_sampling, pixel_sampling = pixel_sampling)
     # split test and validation set
     test_size = 0.5
     test_set_size = int(len(test_val_set) * test_size)
