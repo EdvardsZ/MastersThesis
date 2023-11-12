27f977c22e491f7356e471c869a723db9eeddb36
EdvardsZ <edvards1999@gmail.com>
2023-11-12 18:52:10 +0100

    Fixed random pixel sampler

--- src/datasets/observations/pixel_sampling/random_pixel_sampler.py
+++ src/datasets/observations/pixel_sampling/random_pixel_sampler.py
@@ -6,15 +6,16 @@ class RandomPixelSampler(PixelSampler):
         self.add_mask = add_mask
 
     def sample(self, image: torch.Tensor, pixel_count: int) -> torch.Tensor:
-        zeros = torch.zeros((pixel_count))
-        indices = torch.randint_like(zeros, 0, image.shape[1] * image.shape[2], dtype=torch.long) # hmm this is not quite right because the same pixel can be sampled twice
+        zeros = torch.zeros((pixel_count, 2))
+        indices = torch.randint_like(zeros, 0, image.shape[1], dtype=torch.long) # hmm this is not quite right because the same pixel can be sampled twice
 
         sampled_pixels = torch.zeros_like(image)
-        sampled_pixels.view(-1)[indices] = image.view(-1)[indices]
+
+        sampled_pixels[:, indices[:, 0], indices[:, 1]] = image[:, indices[:, 0], indices[:, 1]]
         
         if self.add_mask:
             mask = torch.zeros_like(image[0:1, :, :]) 
-            mask.view(-1)[indices] = 1
+            mask[:, indices[:, 0], indices[:, 1]] = 1
             sampled_pixels = torch.cat([sampled_pixels, mask], dim=0)
 
 
