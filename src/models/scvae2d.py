0a476e4cbaa7a81a66c6bc3ce87cbde73730f772
EdvardsZ <edvards1999@gmail.com>
2023-11-12 23:42:47 +0100

    update

--- src/models/scvae2d.py
+++ src/models/scvae2d.py
@@ -17,21 +17,13 @@ class SCVAE2D(nn.Module):
         self.loss = VAELoss(weight_kl=1.0)
 
     def forward(self, x, x_cond, y):
-        print("TEST")
         z, z_mean, z_log_var = self.encoder(x)
 
-        print("z shape: ", z.shape)
-
-
         output_1 = self.decoder(z)
-
-        print("output 1 shape: ", output_1.shape)
 
         x_cat = concat_latent_with_cond(z, x_cond)
 
         output_2 = self.pixel_decoder(x_cat)
-
-        print("output 2 shape: ", output_2.shape)
 
         return [output_1, output_2], z, z_mean, z_log_var
 
