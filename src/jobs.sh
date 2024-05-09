692f142d73b9dd0de39c4ab36d1ca3aa2a2d05af
EdvardsZ <edvards1999@gmail.com>
2024-05-09 19:58:10 +0200

    script update

--- src/jobs.sh
+++ src/jobs.sh
@@ -10,8 +10,8 @@ do
     nohup python train_all.py --device $index --dataset CelebA > CelebA_job_$index.out 2>&1 &
     echo "Started Python job $index"
 
-    # Wait 30 seconds before starting the job
-    sleep 30
+    # Wait 15 seconds before starting the job
+    sleep 15
 done
 
 echo "All Python jobs started successfully"
